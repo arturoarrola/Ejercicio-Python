860beccff5e8a6e6e03ae2e986555b352d9bfd3a
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2022-11-28 21:35:27 -0800

    Create 6_15par,inpar,flores.py

---
+++ 6_15par,inpar,flores.py
@@ -0,0 +1,8 @@
+def lovefunc( flower1, flower2 ):
+    if flower1 == 1, 3 ,5 ,7, 9 and flower2 == 0, 2, 4, 6, 8:
+        return True
+    elif flower1 == 0, 2, 4, 6, 8 and flower2 == 1, 3 ,5 ,7, 9:
+        return True
+    else:
+        return False
+      invalid tax
