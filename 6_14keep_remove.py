f21a962ab9e2fa8049603129631a1b908f0f326d
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2022-11-28 21:20:04 -0800

    Create 6_14keep_remove.py

---
+++ 6_14keep_remove.py
@@ -0,0 +1,6 @@
+def remove_every_other(my_list):
+    for array in my_list:
+        (my_list[-1] = 0, 2, 4, 6, 8) = "Keep"
+        (my_list[-1] = 1, 3, 5, 7, 9) = "Remove"
+    return Keep and Remove
+    no me ha salido, era el de keep move
