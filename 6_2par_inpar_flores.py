bb52cb4552054556845a328ef15628f6450d992f
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2022-12-01 22:09:42 -0800

    Update and rename 6_15par,inpar,flores.py to 6_2par_inpar_flores.py

---
+++ 6_2par_inpar_flores.py
@@ -0,0 +1,17 @@
+def lovefunc( flower1, flower2 ):
+    if flower1 == 1, 3 ,5 ,7, 9 and flower2 == 0, 2, 4, 6, 8:
+        return True
+    elif flower1 == 0, 2, 4, 6, 8 and flower2 == 1, 3 ,5 ,7, 9:
+        return True
+    else:
+        return False
+      invalid taxdef lovefunc( flower1, flower2 ):
+    
+    
+    if flower1 == 1 or 3 or 5 or 7 or 9 and flower2 == 0 or 2 or 4 or 6 or 8:
+        return True
+    elif flower1 == 0 or 2 or 4 or 6 or 8 and flower2 == 1 or 3 or 5 or 7 or 9:
+        return True
+    else:
+        return False
+    niguna es pero por si acaso.
