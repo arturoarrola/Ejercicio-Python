28fa31cd6d5aae8322c19e86f78f141f84fa6bb7
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2022-12-06 22:28:17 -0800

    Update 6_1keep_keep_move.py

--- 6_1keep_keep_move.py
+++ 6_1keep_keep_move.py
@@ -1,6 +1,10 @@
 def remove_every_other(my_list):
-    for array in my_list:
-        (my_list[-1] = 0, 2, 4, 6, 8) = "Keep"
-        (my_list[-1] = 1, 3, 5, 7, 9) = "Remove"
-    return Keep and Remove
-    no me ha salido, era el de keep move
+    keep=[]
+    par = True
+    for num in my_list:
+        if par == True:
+            keep.append(num)
+            par = False
+        else:
+            par =True
+    return keep
