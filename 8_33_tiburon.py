21220dc60c6cd50045f5c8d6de4d7c85b8b20ed0
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2022-12-12 23:03:34 -0800

    Update 8_33_tiburon.py

--- 8_33_tiburon.py
+++ 8_33_tiburon.py
@@ -1,1 +1,9 @@
-
+def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
+    if dolphin == True:
+        shark_speed/=2
+    sharktime = shark_distance/shark_speed
+    mytime = pontoon_distance/you_speed
+    if  sharktime < mytime:
+        return "Shark Bait!"
+    else:
+        return "Alive!"
