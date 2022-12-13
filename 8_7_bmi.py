d4f656e523f2e47b3e055c6479717b8c36d6c99b
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2022-12-12 22:56:18 -0800

    Create 8_7_bmi.py

---
+++ 8_7_bmi.py
@@ -0,0 +1,10 @@
+def bmi(weight, height):
+    bmi = weight/height**2
+    if bmi <= 18.5:
+        return "Underweight"
+    elif bmi <= 25.0:
+        return "Normal"
+    elif bmi <= 30.0:
+        return "Overweight"
+    elif bmi > 30:
+        return "Obese"
