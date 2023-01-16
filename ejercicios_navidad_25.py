c6c2ee2d402c5a002d0a466aa4c1c2c991e08098
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2023-01-15 19:20:01 -0800

    Create ejercicios_navidad_25.py

---
+++ ejercicios_navidad_25.py
@@ -0,0 +1,9 @@
+def quarter_of(month):
+    if month == 1 or 2 or 3:
+        return 1
+    elif month == 4 or 5 or 6:
+        return 2
+    elif month == 7 or 8 or 9:
+        return 3
+    else:
+        return 4
