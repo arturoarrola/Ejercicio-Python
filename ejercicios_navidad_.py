b29fedf99743ab238f0b432689454c7d20fdcfda
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2023-01-17 22:16:58 -0800

    Create ejercicios_navidad_.py

---
+++ ejercicios_navidad_.py
@@ -0,0 +1,19 @@
+def minimum(arr):
+    total = 0
+    numb = 0
+    min = 5050050050050505050505050505050505050505050550050505
+    for num in arr:
+        if min > arr[numb]:
+            total =arr[numb]
+            min = total
+            numb + 1
+    return min
+def maximum(arr):
+    total
+    numb = 0
+    max = 0
+    for num in arr:
+            total = arr[numb]
+            max  = total
+            numb + 1
+    return max
