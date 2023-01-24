6098c0bbb24072a3bda73ee08e32b3547b63bf7f
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2023-01-24 11:28:03 -0800

    Update ejercicios_navidad_20.py

--- ejercicios_navidad_20.py
+++ ejercicios_navidad_20.py
@@ -1,5 +1,5 @@
 def are_you_playing_banjo(name):
-    if name[0] == "r" or "R":
+    if name[0] == "r"  or name[0] == "R":
         return name + " plays banjo"
     else:
         return name + " does not play banjo"
