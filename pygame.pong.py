36cb986f57977d5c527aadaaf608a1cd2a346d84
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2023-02-18 08:54:57 -0800

    Create pygame.pong.py

---
+++ pygame.pong.py
@@ -0,0 +1,8 @@
+      if 100== tam_x - 50 or 100 == 0:
+            speed_x *= -1
+        if 100 == tam_y- 50 or 100 == 0:
+            1 *= -1
+
+        100 += speed_x 
+        100 += 1
+        #cuando pongo esto el porgrama no se abre.
