6a17bc6cfb2183384d055951ffeaf72f056dae66
arturoarrola <117538345+arturoarrola@users.noreply.github.com>
2023-02-18 08:57:28 -0800

    Update pygame.pong.py

--- pygame.pong.py
+++ pygame.pong.py
@@ -1,4 +1,19 @@
-      if 100== tam_x - 50 or 100 == 0:
+     import sys, pygame
+pygame.init()
+tam_x=800
+speed_x=0
+tam_y=400
+screen = pygame.display.set_mode([tam_x, tam_y])
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        rec_1=pygame.draw.rect(screen, "White",(100,200,25,100))
+        rec_2=pygame.draw.rect(screen, "White",(700,200,25,100))
+        my_circle=pygame.draw.circle(screen, "white", (400,200),10)
+
+ #cuando pongo esto el porgrama no se abre.
+     if 100== tam_x - 50 or 100 == 0:
             speed_x *= -1
         if 100 == tam_y- 50 or 100 == 0:
             1 *= -1
